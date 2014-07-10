68d48bf7e63ce3cbac594b96cb5d2563f957630f
ristinolla <perttu.ristimella@gmail.com>
2014-07-10 10:19:46 +0300

    wrap embedded media to figures
    copied from previous roots

--- lib/extras.php
+++ lib/extras.php
@@ -20,3 +20,55 @@ function roots_wp_title($title) {
   return $title;
 }
 add_filter('wp_title', 'roots_wp_title', 10);
+
+
+
+/**
+ * Wrap embedded media as suggested by Readability
+ *
+ * @link https://gist.github.com/965956
+ * @link http://www.readability.com/publishers/guidelines#publisher
+ */
+function roots_embed_wrap($cache, $url, $attr = '', $post_ID = '') {
+  return '<div class="entry-content-asset">' . $cache . '</div>';
+}
+add_filter('embed_oembed_html', 'roots_embed_wrap', 10, 4);
+
+/**
+ * Add Bootstrap thumbnail styling to images with captions
+ * Use <figure> and <figcaption>
+ *
+ * @link http://justintadlock.com/archives/2011/07/01/captions-in-wordpress
+ */
+function roots_caption($output, $attr, $content) {
+  if (is_feed()) {
+    return $output;
+  }
+
+  $defaults = array(
+    'id'      => '',
+    'align'   => 'alignnone',
+    'width'   => '',
+    'caption' => ''
+  );
+
+  $attr = shortcode_atts($defaults, $attr);
+
+  // If the width is less than 1 or there is no caption, return the content wrapped between the [caption] tags
+  if ($attr['width'] < 1 || empty($attr['caption'])) {
+    return $content;
+  }
+
+  // Set up the attributes for the caption <figure>
+  $attributes  = (!empty($attr['id']) ? ' id="' . esc_attr($attr['id']) . '"' : '' );
+  $attributes .= ' class="wp-caption ' . esc_attr($attr['align']) . '"';
+  //$attributes .= ' style="width: ' . esc_attr($attr['width']) . 'px"';
+
+  $output  = '<figure' . $attributes .'>';
+  $output .= do_shortcode($content);
+  $output .= '<figcaption class="caption wp-caption-text">' . $attr['caption'] . '</figcaption>';
+  $output .= '</figure>';
+
+  return $output;
+}
+add_filter('img_caption_shortcode', 'roots_caption', 10, 3);
