859acd355f452997063408d3f60b9272acb3e1e1
ristinolla <perttu.ristimella@gmail.com>
2014-08-06 17:23:18 +0300

    author site done

--- template-about.php
+++ template-about.php
@@ -3,18 +3,30 @@
 Template Name: Profile Page
 */
 ?>
-<div style="background-image: url(assets/img/test/test5.jpg);"
-  data-spy="pr-herounit"
-  data-hero-max="600"
-  data-hero-treshold="80"
-  class="hero hero-huge">
-</div>
 
-<div class="content container">
-    <?php while (have_posts()) : the_post(); ?>
+
+
+<?php while (have_posts()) : the_post(); ?>
+
+    <?php
+         $heros = get_post_custom_values('hero_img_url', get_the_ID());
+         $hero_img_url = $heros[0];
+    ?>
+
+    <div style="background-image: url(<?php echo $hero_img_url; ?> );"
+      data-spy="pr-herounit"
+      data-hero-max="600"
+      data-hero-treshold="80"
+      class="hero hero-huge">
+    </div>
+
+
+
+    <div class="content container">
       <?php get_template_part('templates/content', 'profile'); ?>
-    <?php endwhile; ?>
-</div>
+    </div>
+<?php endwhile; ?>
+
 
 <div class="full-width">
     <h2 class="text-center"><?php printf(__('Latest posts by %1&s', 'roots'), get_the_author_meta('first_name')); ?></h2>
