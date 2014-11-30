cb78ba9094b4f8b9ba53a2a8f53b5aff413d5e89
ristinolla <perttu.ristimella@gmail.com>
2014-12-01 01:04:07 +0200

    avatar fix

--- templates/author-avatar.php
+++ templates/author-avatar.php
@@ -1,3 +1,10 @@
+<?
+ /*
 <a class="author-avatar" rel="author" href="<?php echo esc_url( souvvi_get_author_url() ); ?>">
 	<img src="<?php echo xo_user_avatar_url(get_the_author_meta('ID'), 'thumbnail'); ?>" alt="" >
+</a>
+*/
+ ?>
+<a class="author-avatar" rel="author" href="<?php echo esc_url( souvvi_get_author_url() ); ?>">
+	<?php echo get_avatar( get_the_author_meta( 'ID' ), 50 ); ?>
 </a>
