46d7b9e7a974d9577362da1d3fa2781ec329af6a
ristinolla <perttu.ristimella@gmail.com>
2014-12-04 23:03:36 +0200

    atuhor image on author page

--- templates/content-profile-2.php
+++ templates/content-profile-2.php
@@ -21,7 +21,9 @@
 
 	<div class="header-content">
 
-		<?php get_template_part('templates/author', 'avatar'); ?>
+		<a class="author-avatar" rel="author" href="<?php echo esc_url( souvvi_get_author_url() ); ?>">
+			<img src="<?php echo xo_user_avatar_url(get_the_author_meta('ID'), 'thumbnail'); ?>" alt="" >
+		</a>
 
 		<h1><?php the_title(); ?></h1>
 
